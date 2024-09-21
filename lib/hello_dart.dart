07400dfc230ee798c9b8501f28b198e35af7eee2
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-21 21:37:34 +0100

    feat: add named parameters experiment

--- lib/hello_dart.dart
+++ lib/hello_dart.dart
@@ -62,3 +62,28 @@ String joinWithCommas(int u, [int? v, int? w, int? x, int? y, int? z]){
   if(z != null) result += ',$z';
   return result;
 }
+
+
+class MyDataObject {
+  final int anInt;
+  final String aString;
+  final double aDouble;
+
+  MyDataObject({
+     this.anInt = 1,
+     this.aString = 'Old!',
+     this.aDouble = 2.0,
+  });
+
+  MyDataObject copyWith({
+    int? newInt,
+    String? newString,
+    double? newDouble
+  }){
+    return MyDataObject(
+      anInt: newInt ?? anInt, 
+      aString: newString ?? aString, 
+      aDouble: newDouble ?? aDouble, 
+    );
+  }
+}
