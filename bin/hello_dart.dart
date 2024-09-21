bc9d05894cb042923f55685bbf95a16b146a6349
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-09-21 12:01:46 +0100

    feat: factorial function

--- bin/hello_dart.dart
+++ bin/hello_dart.dart
@@ -2,4 +2,13 @@ import 'package:hello_dart/hello_dart.dart' as hello_dart;
 
 void main(List<String> arguments) {
   print('Hello world: ${hello_dart.calculate()}!');
+
+  print('');
+  print('factorials');
+  print('---------------------------------');
+  for(int i = 0; i < 10; i++){
+    print('facotrial($i) -> ${hello_dart.factorial(i)}');
+  }
+  print('---------------------------------');
+  
 }
--- lib/hello_dart.dart
+++ lib/hello_dart.dart
@@ -1,3 +1,7 @@
 int calculate() {
   return 6 * 7;
 }
+
+int factorial(int n){
+  return n == 0 ? 1 : n * factorial(n - 1);
+}
