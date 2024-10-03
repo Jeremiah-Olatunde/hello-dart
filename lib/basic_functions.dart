49be6421b9f5ce00efbd373243de19adee9cfb4f
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-03 08:29:20 +0100

    feat(lib basic_functions): add basic arithmetic functions

---
+++ lib/basic_functions.dart
@@ -0,0 +1,15 @@
+int add(int addend, int auguend){
+	return addend + auguend;
+}
+
+int sub(int subtrahend, int minuend){
+	return subtrahend - minuend;
+}
+
+int mul(int multiplicand, int multiplier){
+	return multiplicand * multiplier;
+}
+
+int div(int dividend, int divisor){
+	return dividend ~/ divisor;
+}
