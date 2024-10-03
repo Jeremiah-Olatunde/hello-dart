babbc980e62f8ee69015855893c344ef99494fa7
Jeremiah Olatunde <jesuseun.j.olatunde@gmail.com>
2024-10-03 08:31:03 +0100

    feat(lib basic_functions): add fibonnaci

--- lib/basic_functions.dart
+++ lib/basic_functions.dart
@@ -18,3 +18,8 @@ int factorial(int n){
 	if(n == 0) return 1;
 	return n * factorial(n - 1);
 }
+
+int fibonnaci(int n){
+	if(n == 0 || n == 1) return 1;
+	return fibonnaci(n - 1) + fibonnaci(n - 2);
+}
